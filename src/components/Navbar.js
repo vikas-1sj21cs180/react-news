71682dba60efd9f0ca2819e66bdb313487de0b55
vikas 1sj21cs180 <vv9077022@gmail.com>
2024-09-27 22:08:18 +0530

    image updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Navbar() {
+function Navbar(){
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-         <div className="container-fluid">
+        <div className="container-fluid">
         <Link className="navbar-brand"to="#">Navbar</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
